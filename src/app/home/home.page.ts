59f544fee63e9907f66b0d6e6cb713dd861d2eb1
CETECOM <CETECOM@AVAWSV01LC0612.duoc.acad>
2025-01-15 17:01:35 -0300

    2
    movimiento de icono con ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +9,26 @@ import { NavigationExtras, Router } from '@angular/router';
   standalone: false,
 })
 export class HomePage {
+
+  //JSON
   user = {
     usuario: '',
     password: '',
   };
   msj = "";
   carga = false;
-  constructor(private router: Router) { }
+
+  constructor(private router: Router, private animation: AnimationController) { }
 
 
+//Boton conectar con router a Perfil
 
   conectar() {
+
     if (this.user.usuario.length > 0 && this.user.password.length > 0) {
       if (this.user.usuario == "sevian" && this.user.password == "1234") {
 
+//spiner de carga simulando delay con MS
 
         let navigationExtras: NavigationExtras = {
           state: { user: this.user },
@@ -42,4 +49,55 @@ export class HomePage {
       this.msj = 'Credenciales no pueden estar vacias';
     }
   }
+
+ngAfterContentInit(){
+  this.animacionLogin();
+}
+
+animacionLogin(){
+const imagen = document.querySelector(
+  '#container ion-card ion-card-header ion-img'
+) as HTMLElement;
+
+const animacion = this.animation
+.create()
+.addElement(imagen)
+.duration(3000)
+.iterations(Infinity)
+.keyframes([
+  {
+    offset: 0,
+    opacity: '1',
+    border: '10px solid white',
+    transform: 'translateY(100px)',
+  },
+  {
+    offset: 0.25,
+    opacity: '0.5',
+    border: '10px solid red',
+    transform: 'translateX(100px)',
+  },
+  {
+    offset: 0.5,
+    opacity: '1',
+    border: '10px solid blue',
+    transform: 'translateY(-100px)',
+  },
+  {
+    offset: 0.75,
+    opacity: '1',
+    border: '10px solid green',
+    transform: 'translateX(-100px)',
+  },
+  {
+    offset: 1,
+    opacity: '1',
+    border: '10px solid cyan',
+    transform: 'translateY(100px)',
+  },
+  
+]);
+animacion.play();
+  }
+
 }

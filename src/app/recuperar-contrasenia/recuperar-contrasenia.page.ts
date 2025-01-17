feeb433b0dd9e9be6e2d169d6c302670e63cf28c
pzetta <zanottipaolo8@gmail.com>
2025-01-17 16:47:58 -0300

    animación logo recuperar-contrasenia
    animación logo recuperar-contrasenia

--- src/app/recuperar-contrasenia/recuperar-contrasenia.page.ts
+++ src/app/recuperar-contrasenia/recuperar-contrasenia.page.ts
@@ -73,7 +73,6 @@ export class RecuperarContraseniaPage {
       if (this.user.usuario.length > 0 && this.user.password.length > 0) {
         if (this.user.usuario == "sevian") {
           if (this.user.password != "1234") {
-
       this.carga = true;
       this.msj = 'Éxito! Contraseña cambiada...';
 
@@ -91,17 +90,21 @@ export class RecuperarContraseniaPage {
       }
       } else {
         this.msj = 'No puedes dejar campos vacíos';
-      } 
   }
+}
 
 ngAfterContentInit(){
   this.animacionLogin();
 }
 
 animacionLogin(){
-const imagen = document.querySelector(
-  '#container ion-card ion-card-header ion-img'
-) as HTMLElement;
+  const imagen = document.querySelector(
+    '#container ion-card ion-card-header ion-img'
+  ) as HTMLElement;
+  
+  const fondo = document.querySelector(
+    '#logoDuoc'
+  ) as HTMLElement;
 
 const animacion = this.animation
 .create()
@@ -114,12 +117,40 @@ const animacion = this.animation
     border: '10px 	solid #C21E56',
     //transform: 'translateY(0px)',
   },
- 
- 
-  
+])
+;
+
+const animacionFondo = this.animation
+.create()
+.addElement(fondo)
+.duration(9000)  // Duración de la animación en milisegundos
+.iterations(Infinity) // Animación infinita
+.keyframes([
+  {
+    offset: 0,              // Al principio de la animación
+    transform: 'translateX(-70px) scale(1)',  // Mueve la imagen y tamaño original
+  },
+  {
+    offset: 0.25,           // A los 25% de la animación
+    transform: 'translateX(55px) scale(1.2)',  // La imagen se mueve a la derecha y se agranda un 20%
+  },
+  {
+    offset: 0.5,            // A la mitad de la animación
+    transform: 'translateX(-70px) scale(1)',  // Vuelve a la posición original y tamaño original
+  },
+  {
+    offset: 0.75,           // A los 75% de la animación
+    transform: 'translateX(90px) scale(0.8)',  // Se mueve a la derecha y se achica un 20%
+  },
+  {
+    offset: 1,              // Al final de la animación
+    transform: 'translateX(-70px) scale(1)',  // Vuelve a la posición original y tamaño original
+  }
 ]);
+animacionFondo.play();
+
 animacion.play();
   }
 
-}
 
+}

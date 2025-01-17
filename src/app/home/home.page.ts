6ca0a135f4d978e2b6ae429889285decb22e4c71
CETECOM <CETECOM@AVAWSV01LC0609.duoc.acad>
2025-01-17 15:55:56 -0300

    imagenes
    imagen de fondo con logo duoc y moviemiento

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -77,6 +77,10 @@ const imagen = document.querySelector(
   '#container ion-card ion-card-header ion-img'
 ) as HTMLElement;
 
+const fondo = document.querySelector(
+  '#logoDuoc'
+) as HTMLElement;
+
 const animacion = this.animation
 .create()
 .addElement(imagen)
@@ -88,11 +92,45 @@ const animacion = this.animation
     border: '10px 	solid #C21E56',
     //transform: 'translateY(0px)',
   },
- 
- 
-  
+])
+;
+// Animación para la imagen de fondo
+const animacionFondo = this.animation
+.create()
+.addElement(fondo)
+.duration(9000)  // Duración de la animación en milisegundos
+.iterations(Infinity) // Animación infinita
+.keyframes([
+  {
+    offset: 0,              // Al principio de la animación
+    transform: 'translateX(-70px) scale(1)',  // Mueve la imagen y tamaño original
+  },
+  {
+    offset: 0.25,           // A los 25% de la animación
+    transform: 'translateX(55px) scale(1.2)',  // La imagen se mueve a la derecha y se agranda un 20%
+  },
+  {
+    offset: 0.5,            // A la mitad de la animación
+    transform: 'translateX(-70px) scale(1)',  // Vuelve a la posición original y tamaño original
+  },
+  {
+    offset: 0.75,           // A los 75% de la animación
+    transform: 'translateX(90px) scale(0.8)',  // Se mueve a la derecha y se achica un 20%
+  },
+  {
+    offset: 1,              // Al final de la animación
+    transform: 'translateX(-70px) scale(1)',  // Vuelve a la posición original y tamaño original
+  }
 ]);
+animacionFondo.play();
+
 animacion.play();
   }
+
+  //ngAfterContentInit(){
+    //this.animacionFondo();
+  //}
+
+
 
 }

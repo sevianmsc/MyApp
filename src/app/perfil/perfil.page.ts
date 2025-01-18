3a31ba62f817fc266dfefc83d65c2f22c0c22cc6
sevianmsc <se.mena@duocuc.cl>
2025-01-18 00:35:38 -0300

    version final
    todo funcionando correctamente, agregado imagen con moviento de login y botones funcionales

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,25 +8,62 @@ import { NavigationExtras, Router } from '@angular/router';
   styleUrls: ['./perfil.page.scss'],
   standalone: false,
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage implements OnInit, AfterViewInit {
+  constructor(private router: Router, private animation: AnimationController) {}
+  user = {
+    usuario: '',
+    password: '',
+  };
+  nombreUsuario = '';
 
-  constructor(private router: Router) { }
-  user={
-    usuario:"",
-    password:""
+  ngOnInit() {
+    // Inicializa el usuario desde el estado de la navegación si existe
+    this.user = history.state.user || { usuario: '', password: '' };
+    this.nombreUsuario = this.user.usuario;
   }
-nombreUsuario= '';
-  ngOnInit() {}
-    ngAfterContentInit(){
-    this.user = history.state.user;
-    this.nombreUsuario = this.user.usuario; 
-    }
-    
 
-    recuperarContrasenia() {
-  this.router.navigate(['/recuperar-contrasenia']);
-      
-      
-    }
+  ngAfterViewInit() {
+    this.animacionAutito(); // Llama la animación después de que los elementos estén disponibles
+  }
 
+  recuperarContrasenia() {
+    this.router.navigate(['/recuperar-contrasenia']);
+  }
+
+  cerrarSesion() {
+    this.router.navigate(['/home']);
+  }
+
+  animacionAutito() {
+    const autito = document.querySelector('#autito') as HTMLElement;
+
+    if (autito) {
+      const animacionAutito = this.animation
+        .create()
+        .addElement(autito)
+        .duration(5000)
+        .iterations(Infinity)
+        .keyframes([
+          {
+            offset: 0,
+            transform: 'translateX(-300px)',
+          },
+          {
+            offset: 0.5,
+            transform: 'translateX(300px)',
+          },
+          {
+            offset: 0.51,
+            transform: 'translateX(300px) scaleX(-1)',
+          },
+          {
+            offset: 1,
+            transform: 'translateX(-300px) scaleX(-1)',
+          },
+        ]);
+      animacionAutito.play();
+    } else {
+      console.error('Elemento #autito no encontrado en el DOM.');
+    }
+  }
 }
